6a88f00d588fa6d78e582c44dbe2027eb077354e
Salvador Gil <salvador.romero@upaep.edu.mx>
2021-05-21 19:44:40 -0500

    Reading File ready

--- app/src/main/java/com/chava/filessearch/MainActivity.kt
+++ app/src/main/java/com/chava/filessearch/MainActivity.kt
@@ -1,34 +1,37 @@
 package com.chava.filessearch
 
 import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.chava.filessearch.databinding.ActivityMainBinding
 import com.chava.filessearch.models.File
-import com.chava.filessearch.models.RealPathUtil.getRealPath
 import com.chava.filessearch.models.getFilePath
-import com.google.android.material.button.MaterialButton
-import com.google.android.material.textview.MaterialTextView
+import com.google.android.material.snackbar.Snackbar
+
 
 @Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
-    var fileName: MaterialTextView? = null
-    var words:MaterialTextView? = null
+    val READ_STORAGE_RQ = 101
+    val WRITE_STORAGE_RQ = 102
     val PICK_FILE = 1
-    var filters: MaterialButton? = null
-    var searchFile: MaterialButton? = null
     var file: File? = null
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        filters = findViewById<MaterialButton>(R.id.filters)
-        searchFile = findViewById<MaterialButton>(R.id.searchFile)
-        filters!!.setOnClickListener {
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        checkForPremissions(android.Manifest.permission.READ_EXTERNAL_STORAGE,"Read Permission",READ_STORAGE_RQ)
+        checkForPremissions(android.Manifest.permission.WRITE_EXTERNAL_STORAGE,"Write Permission",WRITE_STORAGE_RQ)
+        binding.filters.setOnClickListener {
             deployFilters()
         }
-        searchFile!!.setOnClickListener{
+        binding.searchFile.setOnClickListener{
             openFile()
-
         }
     }
 
@@ -43,11 +46,55 @@ class MainActivity : AppCompatActivity() {
         }
         startActivityForResult(intent,PICK_FILE)
     }
+    private fun checkForPremissions(permission: String, name: String, requestCode: Int){
+        if(Build.VERSION.SDK_INT>= Build.VERSION_CODES.M){
+            when{
+                ContextCompat.checkSelfPermission(applicationContext,permission) == PackageManager.PERMISSION_GRANTED -> {
+                    Snackbar.make(this@MainActivity,binding.filters,"$name already is granted Thank you!",Snackbar.LENGTH_LONG).setBackgroundTint(getColor(R.color.dark_blue)).setTextColor(getColor(R.color.pure_light)).show()
+                }
+                shouldShowRequestPermissionRationale(permission)->showDialog(permission,name,requestCode)
+                else -> ActivityCompat.requestPermissions(this,arrayOf(permission),requestCode)
+            }
+        }
+    }
 
+    private fun showDialog(permission: String,name: String,requestCode: Int){
+        val builder = AlertDialog.Builder(this)
+
+        builder.apply {
+            setMessage("Permission to access your $name is required to use this app")
+            setTitle("Permission Required")
+            setPositiveButton("Ok"){ dialog, which ->
+                ActivityCompat.requestPermissions(this@MainActivity, arrayOf(permission),requestCode)
+            }
+        }
+        val dialog = builder.create()
+        dialog.show()
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        fun innerCheck(name: String){
+            if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED){
+                Snackbar.make(this@MainActivity,binding.filters,"$name permission refused",Snackbar.LENGTH_LONG).setBackgroundTint(getColor(R.color.dark_blue)).setTextColor(getColor(R.color.pure_light)).show()
+            } else {
+                Snackbar.make(this@MainActivity,binding.filters,"$name permission already granted", Snackbar.LENGTH_LONG).setBackgroundTint(getColor(R.color.dark_blue)).setTextColor(getColor(R.color.pure_light)).show()
+            }
+        }
+
+        when(requestCode){
+            READ_STORAGE_RQ -> innerCheck("Read Storage")
+        }
+    }
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         if(PICK_FILE==requestCode && resultCode== RESULT_OK){
             file = File(data!!.getFilePath(this@MainActivity))
-            Toast.makeText(this@MainActivity,file!!.path,Toast.LENGTH_LONG).show()
+            Snackbar.make(this@MainActivity,binding.filters,file!!.path,Snackbar.LENGTH_LONG).setBackgroundTint(getColor(R.color.dark_blue)).setTextColor(getColor(R.color.pure_light)).show()
+            Snackbar.make(this@MainActivity,binding.filters,"El archivo contiene " + file!!.words.size + " palabras identificadas!",Snackbar.LENGTH_LONG).setBackgroundTint(getColor(R.color.dark_blue)).setTextColor(getColor(R.color.pure_light)).show()
         }
         super.onActivityResult(requestCode, resultCode, data)
 
